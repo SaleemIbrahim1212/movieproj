9658cf196c3037120b09b20cdd59b651cb33deb4
SaleemIbrahim1212 <saleemibrahim673@gmail.com>
2022-05-26 14:35:38 -0400

    First phase of proj

---
+++ src/MovieCard.js
@@ -0,0 +1,32 @@
+
+import react from 'react'
+import {Card} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import images from './images.png'
+import './Container.css'
+import Amazon from './prime.png'
+import Youtube from './youtube.png'
+import Netflix from './netflix.png'
+
+
+function MovieCard (props)
+{
+ return (
+<div className= "Cardz">
+    <Card style={{ width: '18rem', backgroundColor: 'black', height: '-webkit-fill-available'}}>
+        <Card.Img variant="top" src=  {props.Poster} height='400px' width='400px'/>
+        <Card.Body style ={{justifyContent: 'center'}}>
+            <img className="Images" src={Netflix} width="50" height="50" borderRadius = "50%" margin= " 10px"/>
+            <img className="Images" src = {Youtube} width="50" height="50" borderRadius = "50%" margin= " 10px"/>
+            <img className="Images" src = {Amazon} width = "50" height= "50" borderRadius = "50%" margin= " 10px"/>
+        </Card.Body>
+    </Card>
+</div>
+
+
+ );
+}
+
+export default MovieCard;
+
+
