71b204ef4ad49adb57ea65f4faa2d1a250f07cac
SaleemIbrahim1212 <saleemibrahim673@gmail.com>
2022-05-29 14:09:32 -0400

    Added the search functionality

--- src/App.js
+++ src/App.js
@@ -6,86 +6,42 @@ import Form from 'react-bootstrap/Form'
 import {FormControl} from "react-bootstrap";
 import Button from 'react-bootstrap/Button'
 import MovieCard from './MovieCard'
-import react from "react";
+import react, {useState} from "react";
 import ReactDOM from 'react-dom';
+import {useEffect} from "react";
 
 function App() {
 
-  const MovieData = [
+    const [Searchs, UpdateSearch] = useState(`http://www.omdbapi.com/?s=the punisher&apikey=2cd52f36`);
+    const [MovieData, setMovies] = useState([]);
 
-      {
-          "Title": "Enter the Anime",
-          "Year": "2019",
-          "imdbID": "tt10662450",
-          "Type": "movie",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwNGI5MTAtMGJlZS00MzZlLWJjNmItZDcxYTFmY2VmMGJlXkEyXkFqcGdeQXVyMjUxMTEwNTE@._V1_SX300.jpg"
-      },
-      {
-          "Title": "Weird Anime Excel Saga",
-          "Year": "1999–2001",
-          "imdbID": "tt0240493",
-          "Type": "series",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BMTIxMTM5MDY4NF5BMl5BanBnXkFtZTYwODcyNDk5._V1_SX300.jpg"
-      },
-      {
-          "Title": "Tokyo Ghoul: Re - Anime",
-          "Year": "2018",
-          "imdbID": "tt8046356",
-          "Type": "movie",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BNGU5ZDA0MDUtMDBlYS00OGIyLThjYTYtMmRkODc0MGYyNDE5XkEyXkFqcGdeQXVyMjI2ODkzODY@._V1_SX300.jpg"
-      },
-      {
-          "Title": "Marvel Anime",
-          "Year": "2010–2011",
-          "imdbID": "tt1945805",
-          "Type": "series",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BMzM2M2M2MmItYjE3NS00YmNhLTkyZmItYzNkZWJiYzJmYjg0XkEyXkFqcGdeQXVyNjExODE1MDc@._V1_SX300.jpg"
-      },
-      {
-          "Title": "Anime san jushi",
-          "Year": "1987–1989",
-          "imdbID": "tt0294002",
-          "Type": "series",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BM2IzMThlNTAtNTRjZS00NzlkLThjYjgtOGU3ZmIzNGE5ZjQ3XkEyXkFqcGdeQXVyMzQ4MDczNzQ@._V1_SX300.jpg"
-      },
-      {
-          "Title": "Your Anime Sucks: Sword Art Online II",
-          "Year": "2014–2015",
-          "imdbID": "tt8183792",
-          "Type": "series",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BNDZjZWMxNjItMGI0Yy00MzA1LTg0OGMtOTc1MmEzMDQwZTk2L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjg4NzY5ODc@._V1_SX300.jpg"
-      },
-      {
-          "Title": "Anime Crimes Division",
-          "Year": "2017–",
-          "imdbID": "tt7700928",
-          "Type": "series",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BZTQyZWY5ZWYtNmNlZC00YTJiLWE5OTYtYjllZTc3MWQwYTAwXkEyXkFqcGdeQXVyMTQ4NDAzMDQ@._V1_SX300.jpg"
-      },
-      {
-          "Title": "Anime Abandon",
-          "Year": "2011–",
-          "imdbID": "tt2293415",
-          "Type": "series",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BMDA1YTUzMGItYTlhMS00ODY0LTg3ZmUtMzJjNjM5Zjc1NTQ4XkEyXkFqcGdeQXVyNjg5MjUzMTI@._V1_SX300.jpg"
-      },
-      {
-          "Title": "Mumkey's Anime Reviews",
-          "Year": "2016–2017",
-          "imdbID": "tt8252754",
-          "Type": "series",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BZWIwYjg3NDEtYWRhYi00NjM2LWI4MzUtZTRmYjEyNWFkMjJkXkEyXkFqcGdeQXVyNjM0ODM5MTk@._V1_SX300.jpg"
-      },
-      {
-          "Title": "The SpongeBob SquarePants Anime",
-          "Year": "2020–",
-          "imdbID": "tt12895414",
-          "Type": "series",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BM2ZiY2IxZjgtNGVlMi00ZTQ5LTk4N2EtZDgwNDdkNjU0MWZhXkEyXkFqcGdeQXVyMTEzOTI1NTc1._V1_SX300.jpg"
-      }
+    const getMovieRequest = async () => {
+        const url = Searchs;
+
+        const response = await fetch(Searchs);
+        const responseJson = await response.json();
+
+        if (responseJson.Search) {
+            setMovies(responseJson.Search);
+        }
+    };
+
+    useEffect(() => {
+        getMovieRequest();
+    }, [Searchs]);
+    const getinputfromsearch = (event ) =>
+    {
+        const val = document.getElementById("Forms").value;
+        UpdateSearch(`http://www.omdbapi.com/?s=` + val + `&apikey=2cd52f36` )
+
+    };
 
 
-  ]
+const clicked = () => {
+
+}
+
+
 
   return (
 
@@ -100,12 +56,14 @@ function App() {
         <Form className = "d-flex">
 
             <FormControl
+                id = "Forms"
                 type = "search"
                 placeholder =" Search for movies"
                 className = "me-2"
                 aria-label = "Search"
+
                 />
-            <Button variant = "outline-success">Search</Button>
+            <Button variant = "outline-success" onClick= {getinputfromsearch}>Search</Button>
         </Form>
           </div>
 
@@ -113,7 +71,7 @@ function App() {
       </div>
 
         <div className="center">
-      { MovieData.map( (m) =>  <MovieCard Title = {m.Title} Year = {m.Year} imdbID = {m.imdbID} Type = {m.Type} Poster = {m.Poster}/> ) }
+      { MovieData.map( (m) =>  <MovieCard key = {m.imdbID} Title = {m.Title} Year = {m.Year} imdbID = {m.imdbID} Type = {m.Type} Poster = {m.Poster}/> ) }
 
         </div>
 
--- src/MovieCard.js
+++ src/MovieCard.js
@@ -13,13 +13,13 @@ function MovieCard (props)
 {
  return (
 <div className= "Cardz">
-    <Card style={{ width: '18rem', backgroundColor: 'black', height: '-webkit-fill-available'}}>
+    <Card key = {props.imdbID} style={{ width: '18rem', backgroundColor: 'black', height: '-webkit-fill-available'}}>
         <Card.Img variant="top" src=  {props.Poster} height='400px' width='400px'/>
         <Card.Body style ={{justifyContent: 'center'}}>
-            <img className="Images" src={Netflix} width="50" height="50" borderRadius = "50%" margin= " 10px"/>
-            <img className="Images" src = {Youtube} width="50" height="50" borderRadius = "50%" margin= " 10px"/>
-            <img className="Images" src = {Amazon} width = "50" height= "50" borderRadius = "50%" margin= " 10px"/>
-        </Card.Body>
+            <img className="Images" src={Netflix} width="50" height="50"  margin= " 10px"/>
+            <img className="Images" src = {Youtube} width="50" height="50"   margin= " 10px"/>
+            <img className="Images" src = {Amazon} width = "50" height= "50"   margin= " 10px"/>
+         </Card.Body>
     </Card>
 </div>
 
