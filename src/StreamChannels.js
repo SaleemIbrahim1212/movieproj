0467d1052226845cfe9226b84586affde8b70094
SaleemIbrahim1212 <saleemibrahim673@gmail.com>
2022-06-03 15:22:56 -0400

    Now only shows were show is available

--- src/StreamChannels.js
+++ src/StreamChannels.js
@@ -1,22 +1,82 @@
 import react from "react";
-import images from './images.png'
+import {useState} from "react";
 import './Container.css'
 import Amazon from './prime.png'
 import Youtube from './youtube.png'
 import Netflix from './netflix.png'
-import {AppSearch} from "./APISearch";
-import { nanoid } from 'nanoid'
+
 function Streams (props)
 {
+
+    const [isNetflix, setNetflix] = useState(false )
+    const [isPrime, setPrime ] = useState(false)
+    const [Netflixurl, setNetflixurl] = useState('')
+    const [Primeurl, setPrimeurl] = useState('')
+
+    const [isChecked, setChecked] = useState(false)
+
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com',
+            'X-RapidAPI-Key': '5865186d09msh28f738a14950d04p13e3ddjsn14200648c2e1'
+        }
+    };
+
+
+
+ if (!isChecked) {
+     console.log ("Here")
+     fetch('https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=' + props.Title + '&country=ca', options)
+         .then(response => response.json())
+         .then(response =>
+
+             response.results.map((r) => r.locations.map((e) => {
+
+                     const name = e.display_name
+                     const url = e.url
+
+                     if (name === "Netflix" && isNetflix === false) {
+
+                         setNetflixurl(url)
+                         setNetflix(true)
+
+
+                     }
+                     if (name === "Amazon Prime Video" && isPrime === false) {
+
+                         setPrimeurl(url)
+                         setPrime(true)
+
+                     }
+
+                     setChecked(true)
+
+
+                 }
+             )))
+         .catch(err => console.error(err));
+
+ }
 
     return (
 
 
         <>
 
-        <img className="Images" src={Netflix} width="50" height="50"  margin= " 10px" onClick={ () => AppSearch(props.Title, true,false)} />
+
+            { isNetflix === true ? <img className="Images" src={Netflix} width="50" height="50"  margin= " 10px" onClick={ () => window.open(Netflixurl, '_blank').focus() } />
+             : <div></div>}
+
+
     <img className="Images" src = {Youtube} width="50" height="50"   margin= " 10px" onClick={ () => {window.open("https://www.youtube.com/results?search_query=" + props.Title)}}/>
-    <img className="Images" src = {Amazon} width = "50" height= "50"   margin= " 10px" onClick={ () => AppSearch(props.Title, false,true)}/>
+
+
+            {isPrime === true ?            <img className="Images" src={Amazon} width="50" height="50" margin=" 10px"
+                                                onClick={() => window.open(Primeurl, '_blank').focus() }/> : <div></div> }
+
+
         </>
 )
 
