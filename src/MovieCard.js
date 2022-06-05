e2518d6c15b47794f6abeca9accccce56f02b3fe
SaleemIbrahim1212 <saleemibrahim673@gmail.com>
2022-06-05 14:15:43 -0400

    Content in streams now justified

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -13,10 +13,10 @@ const [flag, setFlag] = useState(false);
  return (
 
      <div className={"item"}>
-    <Card style={{ width: '18rem' , backgroundColor: '#ffffff00'}} className= "hvr-float">
-        <Card.Img variant="top" src =  { props.Poster === "N/A"  ?  nopicture :  props.Poster }  height='400px' width='400px' alt = {props.Title } onClick={() => {setFlag(!flag)}} />
+    <Card style={{ width: '18rem' , backgroundColor: '#ffffff00'}} className= " hvr-float">
+        <Card.Img variant="top hvr-float card" src =  { props.Poster === "N/A"  ?  nopicture :  props.Poster }  height='400px' width='400px' alt = {props.Title } onClick={() => {setFlag(!flag)}} />
         { flag === true ? <Card.Body  >
-            <Streams Title = {props.Title}   Checked = {false}/>
+           <Streams Title = {props.Title}   Checked = {false}/>
          </Card.Body> : ""}
     </Card>
 
